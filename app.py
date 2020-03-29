b7b77e560cb15f38dc53e5fb681552ac5949d2de
jonad <jonadjons@gmail.com>
2020-03-29 07:05:28 -0700

    Added a condition to accommodate short sentences

--- app.py
+++ app.py
@@ -19,10 +19,13 @@ def home():
         src = form.summarization.data.lower()
         headers = {"content-Tupe": "application/json"}
         data = [{"src":src, "id":100}]
-        response = requests.post(URL, json=data, headers=headers)
-        summary = response.text
-        json_response = json.loads(summary)
-        summary = json_response[0][0]['tgt']
+        if len(data[0]['src']) < 10:
+            summary = data[0]['src']
+        else:
+            response = requests.post(URL, json=data, headers=headers)
+            summary = response.text
+            json_response = json.loads(summary)
+            summary = json_response[0][0]['tgt']
     else:
         form.summarization.data = (' ')
     return render_template('home.html', form=form, translation=summary)
@@ -30,4 +33,4 @@ def home():
         
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=3003)
